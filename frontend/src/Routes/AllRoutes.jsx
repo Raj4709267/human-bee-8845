16f5d61f18435c22e5157d969fbf37c8765eefe4
Rahul5303 <rahulnishad977@gmail.com>
2022-11-09 20:40:04 +0530

    chprs

--- frontend/src/Routes/AllRoutes.jsx
+++ frontend/src/Routes/AllRoutes.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import CartPage from '../Pages/CartPage'
+import Homepage from '../Pages/Homepage'
+import Payment from '../Pages/Payment'
+import Review from '../Pages/Review'
+import ShippingAddress from '../Pages/ShippingAddress'
 
 const AllRoutes = () => {
   return (
-    <div>AllRoutes</div>
+    <div>
+      <Routes>
+        <Route path="/" element={<Homepage/>}/>
+        <Route path="/cart" element={<CartPage/>}/>
+        <Route path="/shippingaddress" element={<ShippingAddress/>}/>
+        <Route path="/review" element={<Review/>}/>
+        <Route path="/payment" element={<Payment/>}/>
+      </Routes>
+    </div>
   )
 }
 

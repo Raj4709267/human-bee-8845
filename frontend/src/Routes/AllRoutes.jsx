5428e87950c74f3bd0fd6c5d2953a495d7d37817
Raj kumar mahto <rk2519962@gmail.com>
2022-11-10 07:26:45 +0530

    update files

--- frontend/src/Routes/AllRoutes.jsx
+++ frontend/src/Routes/AllRoutes.jsx
@@ -1,35 +1,35 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import SingleProduct from '../components/SingleProduct/SingleProduct'
-import CartPage from '../Pages/CartPage'
-import Error from '../Pages/Error'
-import Homepage from '../Pages/Homepage'
-import Kids from '../Pages/Kids'
-import Mens from '../Pages/Mens'
-import Payment from '../Pages/Payment'
-import Review from '../Pages/Review'
-import ShippingAddress from '../Pages/ShippingAddress'
-import Wishlist from '../Pages/Wishlist/Wishlist'
-import Womens from '../Pages/Womens'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import SingleProduct from "../Components/SingleProduct/SingleProduct";
+import CartPage from "../Pages/CartPage";
+import Error from "../Pages/Error";
+import Homepage from "../Pages/Homepage";
+import Kids from "../Pages/Kids";
+import Mens from "../Pages/Mens";
+import Payment from "../Pages/Payment";
+import Review from "../Pages/Review";
+import ShippingAddress from "../Pages/ShippingAddress";
+import Wishlist from "../Pages/Wishlist/Wishlist";
+import Womens from "../Pages/Womens";
 
 const AllRoutes = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Homepage/>}/>
-        <Route path="/cart" element={<CartPage/>}/>
-        <Route path="/wishlist" element={<Wishlist/>}/>
-        <Route path="/shippingaddress" element={<ShippingAddress/>}/>
-        <Route path="/review" element={<Review/>}/>
-        <Route path="/payment" element={<Payment/>}/>
-        <Route path="/singleproduct" element={<SingleProduct/>}/>
-        <Route path="/mens" element={<Mens/>}/>
-        <Route path="/womens" element={<Womens/>}/>
-        <Route path="/kids" element={<Kids/>}/>
-        <Route path="*" element={<Error/>}/>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/wishlist" element={<Wishlist />} />
+        <Route path="/shippingaddress" element={<ShippingAddress />} />
+        <Route path="/review" element={<Review />} />
+        <Route path="/payment" element={<Payment />} />
+        <Route path="/singleproduct" element={<SingleProduct />} />
+        <Route path="/mens" element={<Mens />} />
+        <Route path="/womens" element={<Womens />} />
+        <Route path="/kids" element={<Kids />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default AllRoutes
+export default AllRoutes;

9e17faed9111e78378d96aeef887c415f1d1d795
Raj kumar mahto <rk2519962@gmail.com>
2022-11-11 08:43:09 +0530

    main

--- frontend/src/Store/store.js
+++
@@ -1,9 +0,0 @@
-
-import { legacy_createStore, applyMiddleware, combineReducers } from "redux";
-import thunk from "redux-thunk";
-const rootReducer = combineReducers({
-});
-
-const middleware = applyMiddleware(thunk);
-
-export const store = legacy_createStore(rootReducer, middleware);

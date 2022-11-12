060d2db1b9f711f69e0c80a8f6fcb6be7cb4de3c
Raj kumar mahto <rk2519962@gmail.com>
2022-11-12 12:22:25 +0530

    update backend

--- Backend/Middlewares/authentication.js
+++ Backend/Middlewares/authentication.js
@@ -6,14 +6,22 @@ const authentication = (req, res, next) => {
   if (!token) {
     return res.status(401).send({ msg: "plz login again" });
   } else {
-    const decoded = jwt.verify(token, process.env.SECRET_KEY);
-    if (decoded) {
-      req.body.userId = decoded.userId;
-      next();
-    } else {
-      res.status(401).send({ msg: "plz login again" });
-    }
+    jwt.verify(token, process.env.SECRETKEY, function (err, decoded) {
+      if (err) {
+        res.status(401).send({ msg: "plz login again" });
+      } else {
+        req.body.userId = decoded.userId;
+        next();
+      }
+    });
   }
+  // console.log(req.body)
+
+  // if(!req.headers.authorization){
+  //     return res.status(401).send({"msg":'plz login again'})
+  // }
+
+  // const token=req.headers.authorization.split(" ")[1]
 };
 
 module.exports = { authentication };
